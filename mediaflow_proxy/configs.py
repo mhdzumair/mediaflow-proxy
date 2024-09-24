7b91efd8b258559633c1241eac16611e28ce25b2
mhdzumair <mhdzumair@gmail.com>
2024-09-24 09:06:36 +0530

    Reduce live MPD stream delay to 10 seconds

--- mediaflow_proxy/configs.py
+++ mediaflow_proxy/configs.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     api_password: str  # The password for accessing the API endpoints.
     proxy_url: str | None = None  # The URL of the proxy server to route requests through.
-    mpd_live_stream_delay: int = 30  # The delay in seconds for live MPD streams.
+    mpd_live_stream_delay: int = 10  # The delay in seconds for live MPD streams.
 
     class Config:
         env_file = ".env"
