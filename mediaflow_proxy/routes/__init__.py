e7040adf6050ad673afe90a029af7ff6800c97d8
aleZZ2001 <36056536+aleZZ2001@users.noreply.github.com>
2025-05-22 15:27:05 +0200

    Delete mediaflow_proxy directory

--- mediaflow_proxy/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .proxy import proxy_router
-from .extractor import extractor_router
-from .speedtest import speedtest_router
-
-__all__ = ["proxy_router", "extractor_router", "speedtest_router"]
