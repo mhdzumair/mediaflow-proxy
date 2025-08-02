b48aaa40936e24e7ea99b2988d2b85d8a5d2e6db
nzo66 <nzo66.business@gmail.com>
2025-08-02 08:12:59 +0200

    Update dlhd.py

--- mediaflow_proxy/extractors/dlhd.py
+++ mediaflow_proxy/extractors/dlhd.py
@@ -1,3 +1,5 @@
+import ssl
+import certifi
 import re
 import base64
 from typing import Any, Dict, Optional
@@ -62,7 +64,8 @@ class DLHDExtractor(BaseExtractor):
                 'Referer': baseurl,
                 'Origin': daddy_origin
             }
-            async with httpx.AsyncClient(timeout=15, follow_redirects=True, verify=False) as client:
+            ssl_context = ssl.create_default_context(cafile=certifi.where())
+            async with httpx.AsyncClient(timeout=15, follow_redirects=True, verify=ssl_context) as client:
                 # 1. Richiesta alla pagina stream/cast/player/watch
                 resp1 = await client.get(stream_url, headers=daddylive_headers)
                 resp1.raise_for_status()
