92b78958f811a64d74197edee2289abb6eefbeb6
webstreamr <210764791+webstreamr@users.noreply.github.com>
2025-09-09 11:05:11 +0200

    Add FileLions extractor and improve eval_solver (#135)
    * Add FileLions extractor and support for p.a.c.k.e.d pattern array
    
    * Improve eval_solver URL creation
    
    * Support also hls3 link
    
    * Improve eval_resolver failing when no pattern matches
    
    * Improve URL generation a bit more
    
    * Apply more review suggestions
    
    * Further simplify URL creation and fix error message
    
    * Remove hls3 link again because it's not always working

--- mediaflow_proxy/schemas.py
+++ mediaflow_proxy/schemas.py
@@ -96,7 +96,7 @@ class MPDSegmentParams(GenericParams):
 
 class ExtractorURLParams(GenericParams):
     host: Literal[
-        "Doodstream", "Mixdrop", "Uqload", "Streamtape", "Supervideo", "VixCloud", "Okru", "Maxstream", "LiveTV", "DLHD", "Fastream"
+        "Doodstream", "FileLions", "Mixdrop", "Uqload", "Streamtape", "Supervideo", "VixCloud", "Okru", "Maxstream", "LiveTV", "DLHD", "Fastream"
     ] = Field(..., description="The host to extract the URL from.")
     destination: str = Field(..., description="The URL of the stream.", alias="d")
     redirect_stream: bool = Field(False, description="Whether to redirect to the stream endpoint automatically.")
